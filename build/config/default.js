1c5cefd71019c9e483fcd5de1aedf99820e4f19c
luciosacramento <91433765+luciosacramento@users.noreply.github.com>
2022-04-07 16:18:17 -0300

    removendo build folder

--- build/config/default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
-exports.default = {
-    port: 3000,
-    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.mnoyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    env: "development"
-};
