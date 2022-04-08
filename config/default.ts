acc4e39f6ff43ef07078fa61650ab37d5230f4f2
luciosacramento <91433765+luciosacramento@users.noreply.github.com>
2022-04-07 21:38:42 -0300

    git rm -r --cached some-directory

--- config/default.ts
+++
@@ -1,9 +0,0 @@
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
-const port = process.env.PORT || "443";
-
-export default {
-    port: port,
-    dbUri: `mongodb+srv://NinaStella:IZiVG7WLCV5TzYja@cluster0.mnoyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    env: "development"
-}
