ece9311c7d72aad5f68b93837b168b3cad3d55de
luciosacramento <91433765+luciosacramento@users.noreply.github.com>
2022-04-08 08:00:59 -0300

    adicionando js

---
+++ config/default.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+const port = process.env.PORT || "443";
+exports.default = {
+    port: port,
+    dbUri: `mongodb+srv://NinaStella:IZiVG7WLCV5TzYja@cluster0.mnoyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    env: "development"
+};
