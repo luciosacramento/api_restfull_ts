d751e17d6c668dd3384ae984a9b9bc526303090c
luciosacramento <91433765+luciosacramento@users.noreply.github.com>
2022-04-07 22:40:59 -0300

    teste

--- app.ts
+++ app.ts
@@ -14,6 +14,8 @@ app.use(express.json())
 
 app.use(morganMiddleware)
 
+console.log("oi");
+
 //routes
 app.use("/api/", router)
 
@@ -22,6 +24,7 @@ const port = config.get<number>("port")
 
 app.listen(port,async () =>{
     await db()
+    console.log("Express server listening on port %d in %s mode", port, app.settings.env);
     Logger.info(`Aplicação está funcionando na porta: ${port}`)
 })
 
